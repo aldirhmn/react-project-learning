984dc52c075c3eaa7cae0ed54182bdc321fc1cd1
Aldi Rahman <aldirhmn@gmail.com>
2024-03-17 03:38:54 +0700

    Feat: Adding Use Effect Learning in Product Page

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
@@ -32,12 +32,22 @@ const products = [
 
 const email = localStorage.getItem("email");
 const ProductsPage = () => {
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      qty: 1,
-    },
-  ]);
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("cart")) || []);
+  }, []);
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return acc + product.price * item.qty;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
 
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -101,7 +111,6 @@ const ProductsPage = () => {
                   <tr key={item.id}>
                     <td>{product.name}</td>
                     <td>
-                      {" "}
                       Rp{" "}
                       {product.price.toLocaleString("id-ID", {
                         styles: "currency",
@@ -110,6 +119,7 @@ const ProductsPage = () => {
                     </td>
                     <td>{item.qty}</td>
                     <td>
+                      Rp{" "}
                       {(product.price * item.qty).toLocaleString("id-ID", {
                         styles: "currency",
                         currency: "IDR",
@@ -118,6 +128,20 @@ const ProductsPage = () => {
                   </tr>
                 );
               })}
+              <tr>
+                <td colSpan={3}>
+                  <b>Total Price</b>
+                </td>
+                <td>
+                  <b>
+                    Rp{" "}
+                    {totalPrice.toLocaleString("id-ID", {
+                      styles: "currency",
+                      currency: "IDR",
+                    })}
+                  </b>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
